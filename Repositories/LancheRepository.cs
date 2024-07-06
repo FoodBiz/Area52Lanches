a3d3529057ced6cac98465551299d260674d1a5f
wmedrado <williamfranckm6@gmail.com>
2024-07-06 13:57:42 -0300

    Add project files.

---
+++ Repositories/LancheRepository.cs
@@ -0,0 +1,26 @@
+﻿using Area52Lanches.Models;
+using Area52Lanches.Models.Context;
+using Area52Lanches.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Area52Lanches.Repositories
+{
+    public class LancheRepository : ILancheRepository
+    {
+        private readonly AppDbContext _context;
+
+        public LancheRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Lanche> Lanches => _context.Lanches;
+
+        public IEnumerable<Lanche> LanchesPreferidos => _context.Lanches.Where(l => l.IsLanchePreferido);
+
+        public Lanche GetLancheById(int lancheId)
+        {
+            return _context.Lanches.FirstOrDefault(l => l.LancheId == lancheId);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,56 @@
+﻿using Area52Lanches.Models.Context;
+using Area52Lanches.Models;
+using Area52Lanches.Repositories;
+using Area52Lanches.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Area52Lanches
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddTransient<ILancheRepository, LancheRepository>();
+            services.AddTransient<ICategoriaRepository, CategoriaRepository>();
+
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
